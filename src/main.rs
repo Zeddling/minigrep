2daee2ec3507d8bd886c632b4c2ea74b40d6ab98
zeddling <victorzeddys@gmail.com>
2021-06-25 13:42:38 +0300

    Refactoring code to library and binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate minigrep;
+
+use minigrep::Config;
+use minigrep::run;
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
